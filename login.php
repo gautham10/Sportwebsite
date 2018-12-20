80df030a1d27cfe90fcebb7e4abe46792fb8e87b
gautham10 <38566599+gautham10@users.noreply.github.com>
2018-12-20 09:17:08 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,9 @@
+
+login.php
+<?php
+    $host = "127.0.0.1";
+    $user = "gautham2002";
+    $pass = "";
+   $db = "sports";
+    $port = 3306;
+ ?>
---
+++ phptest2.php
@@ -0,0 +1,19 @@
+
+phptest2.php
+	<?php
+		include_once 'formtest.html';
+		require_once 'login.php';
+		$conn = mysqli_connect($host, $user, $pass, $db, $port);
+		if($conn->connect_error) die($conn->connect_error);
+		$option = isset($_POST['myTest']) ? $_POST['myTest'] : false;
+		if ($option){
+			$testField = $_POST['myTest'];
+			$query = "	select ". $testField . " from users;";
+			$result = mysqli_query($conn, $query);
+			while ($row = mysqli_fetch_assoc($result)) {
+				echo $row[$testField] . "<br>";
+			}
+		}
+		$result->close();
+		$conn->close();
+	?>
